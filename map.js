7d15cefb1804a981a4bd3fb9e65c35c4bc2d7173
Suan KC <kcsuan424@gmail.com>
2025-04-29 20:20:35 +0545

    Maps

---
+++ map.js
@@ -0,0 +1,12 @@
+// Mapping the elemets 
+
+let arr = [1,2,4,5,6,7]
+
+const newNum = arr.map((arr)=> arr * 2)
+
+console.log(newNum)
+
+const contacts = new Map()
+contacts.set("Ram", {phone: '9034023223', address: 'tahakhel'});
+contacts.set("Ramaa", {phone: '449034023223', address: 'tahakhel'});
+console.log(contacts)
