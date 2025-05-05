0560aaef1804f6c6e99a041263527319dd1122ae
Suan KC <kcsuan424@gmail.com>
2025-05-05 20:34:46 +0545

    Advance level promises

--- promise.js
+++ promise.js
@@ -110,12 +110,73 @@ promise4.then((user) => {
   console.log(username)
 }).catch((error) => {
   console.log(error)
-}).finally(()=>{
+}).finally(() => {
   console.log("The promise have been resolved or rejected")
 })
 
 
 
 
+const promise5 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: 'suankc', password: 'suan123' })
+    } else {
+      reject("Error: Promise5 went wrong")
+    }
+  }, 1000);
+})
+
+// promise5.then(()=>{})
+// using Async and await to handle the promise 
+
+async function consumePromise5() {
+  try {
+    const response = await promise5
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+consumePromise5()
 
 
+
+// some real world examples 
+
+// async function getUsers() {
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+
+// }
+
+// getUsers()
+
+
+
+// const getallUser = new Promise((resolve, reject)=>{
+//   setTimeout(() => {
+//     const response = fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = response.json()
+//     console.log(data)
+//   }, 2000);
+// })
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+  return response.json()
+})
+.then((data)=>{
+  console.log(data)
+})
+.catch((error)=>{
+  console.log(error)
+})
